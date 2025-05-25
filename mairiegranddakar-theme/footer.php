331ee2772ca61b00bd2edd97fa41d1371bf358f1
miizii45 <miiziihendriix@gmail.com>
2025-05-25 18:52:31 +0200

    Add mairiegranddakar-theme/footer.php 1748191952251

---
+++ mairiegranddakar-theme/footer.php
@@ -0,0 +1,93 @@
+<footer class="main-footer" style="background-image: url(https://mairiegranddakar.com/public/images/background/3.jpg)">
+    	<div class="auto-container">
+        	<!--Widgets Section-->
+            <div class="widgets-section">
+            	<div class="row clearfix">
+
+					<!-- Footer Column -->
+					<div class="footer-column col-lg-4 col-md-6 col-sm-12">
+						<div class="footer-widget logo-widget">
+							<div class="logo">
+								<a href="<?php echo esc_url(home_url('/')); ?>"><img src="https://mairiegranddakar.com/public/images/mairie.png"  alt="" /></a>
+							</div>
+							<ul class="contact-link">
+								<li><span class="icon flaticon-clock"></span> Lundi - Jeudi de 08h 0 17H</li>
+								<li><span class="icon flaticon-clock"></span> Vendredi de 08h 0 13H 30</li>
+								<li><span class="icon flaticon-home-page"></span>Avenue Habib Bourguiba X Rue
+Abébé Bikila Dakar-SENEGAL </li>
+							</ul>
+						</div>
+					</div>
+
+					<!-- Footer Column -->
+					<div class="footer-column col-lg-4 col-md-6 col-sm-12">
+						<div class="footer-widget news-widget">
+							<div class="footer-title">
+								<h4>Liens importants</h4>
+							</div>
+							<!-- Footer Column -->
+							<div class="widget-content">
+								<ul class="contact-link">
+								<li><a style="color: #ccc" href="https://mairiegranddakar.com/partenariat_et_projet/le_bureau_de_developpement_local">
+									<span class="icon flaticon-star"></span>Le bureau de développement local
+								</a></li>
+								<li><a style="color: #ccc" href="https://mairiegranddakar.com/mairie/mot_du_maire">
+									<span class="icon flaticon-star"></span>Mot du maire
+								</a></li>
+								<li><a style="color: #ccc" href="https://mairiegranddakar.com/mairie/nos_services">
+									<span class="icon flaticon-star"></span>Nos services
+								</a></li>
+								<li><a style="color: #ccc" href="https://mairiegranddakar.com/conseil_municipal/les_deliberations">
+									<span class="icon flaticon-star"></span>Les délibérations
+								</a></li>
+								<li><a style="color: #ccc" href="https://mairiegranddakar.com/partenariat_et_projet/les_projets">
+									<span class="icon flaticon-star"></span>Les projets
+								</a></li>
+							</ul>
+							</div>
+						</div>
+					</div>
+
+					<!-- Footer Column -->
+					<div class="footer-column col-lg-4 col-md-6 col-sm-12">
+						<div class="footer-widget newsletter-widget">
+							<div class="footer-title">
+								<h4>Contacts d'urgence</h4>
+							</div>
+							<div class="widget-content">
+								<ul class="contact-link">
+								<li><span class="icon flaticon-security"></span>Police Nationale : <strong>17</strong></li>
+								<li><span class="icon flaticon-security"></span>Sapeur Pompier : <strong>18</strong></li>
+								<li><span class="icon flaticon-security"></span>Gendarmerie : <strong>800 00 20 20</strong></li>
+								<li><span class="icon flaticon-security"></span>SAMU National : <strong>1515</strong></li>
+							</ul>
+							</div>
+							<ul class="social-icon-one">
+								<li class="title">Nous suivre :</li>
+								<li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-skype"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="footer-bottom">
+			<div class="auto-container">
+				<div class="clearfix">
+					<div class="pull-left">
+						<div class="copyright">&copy; 2025 Mairie de Kayemor, Tous droits réservés developpé par HouseTech</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
